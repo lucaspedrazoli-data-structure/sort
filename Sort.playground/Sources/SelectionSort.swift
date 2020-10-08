0032392293c225c60c4526d6de7d97c6018098cf
Lucas <lucaspedrazoli@gmail.com>
2020-10-08 17:50:28 -0300

    make selection sort generic

--- Sort.playground/Sources/SelectionSort.swift
+++ Sort.playground/Sources/SelectionSort.swift
@@ -1,22 +1,23 @@
 import Foundation
 
-public func selectionSort<Element>(_ array: inout [Element])
-  where Element: Comparable {
-    guard array.count >= 2 else {
+public func selectionSort<T>(_ collection: inout T)
+  where T: MutableCollection, T.Element: Comparable {
+    guard collection.count >= 2 else {
       return
     }
 
-    for current in 0..<(array.count - 1) {
+    for current in collection.indices {
       var lowest = current
-      for other in (current + 1)..<array.count {
-        if array[lowest] > array[other] {
+      var other = collection.index(after: current)
+      while other < collection.endIndex {
+        if collection[lowest] > collection[other] {
           lowest = other
         }
+        other = collection.index(after: other)
       }
 
       if lowest != current {
-        array.swapAt(lowest, current)
+        collection.swapAt(lowest, current)
       }
-
     }
 }
