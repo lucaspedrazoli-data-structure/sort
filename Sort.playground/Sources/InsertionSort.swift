f6f10d56cba0e82d37b8b5291a3a7a6bb8d90b86
Lucas <lucaspedrazoli@gmail.com>
2020-10-08 17:54:28 -0300

    make insertion sort generic

--- Sort.playground/Sources/InsertionSort.swift
+++ Sort.playground/Sources/InsertionSort.swift
@@ -1,18 +1,22 @@
 import Foundation
 
-public func insertionSort<Element>(_ array: inout [Element])
-  where Element: Comparable {
-    guard array.count >= 2 else {
+public func insertionSort<T>(_ collection: inout T)
+  where T: BidirectionalCollection & MutableCollection,
+        T.Element: Comparable {
+    guard collection.count >= 2 else {
       return
     }
 
-    for current in 1..<array.count {
-      for shifting in (1...current).reversed() {
-        if array[shifting] < array[shifting - 1] {
-          array.swapAt(shifting, shifting - 1)
+    for current in collection.indices {
+      var shifting = current
+      while shifting > collection.startIndex {
+        let previous = collection.index(before: shifting)
+        if collection[shifting] < collection[previous] {
+          collection.swapAt(shifting, previous)
         } else {
           break
         }
+        shifting = previous
       }
     }
 }
