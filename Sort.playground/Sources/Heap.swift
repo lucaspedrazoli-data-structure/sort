fc7a18a220302f0501c67016baa577df2dea105d
Lucas <lucaspedrazoli@gmail.com>
2020-10-09 19:15:02 -0300

    add heap sort

--- Sort.playground/Sources/Heap.swift
+++ Sort.playground/Sources/Heap.swift
@@ -121,3 +121,14 @@ struct Heap<Element: Equatable> {
     return nil
   }
 }
+
+extension Heap {
+  func sorted() -> [Element] {
+    var heap = Heap(sort: sort, elements: elements)
+    for index in heap.elements.indices.reversed() {
+      heap.elements.swapAt(0, index)
+      heap.siftDown(from: 0, upTo: index)
+    }
+    return heap.elements
+  }
+}
