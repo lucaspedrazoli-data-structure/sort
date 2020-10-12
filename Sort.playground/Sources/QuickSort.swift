00c57f7e82cd30d9513e01277d3280ecf31c3e36
Lucas <lucaspedrazoli@gmail.com>
2020-10-12 17:05:46 -0300

    add quicksort naive and quick sort lomuto

---
+++ Sort.playground/Sources/QuickSort.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+public func quickSortNaive<T: Comparable>(_ a: [T]) -> [T] {
+  guard a.count > 1 else {
+    return a
+  }
+  let pivot = a[a.count / 2]
+  let less = a.filter { $0 < pivot }
+  let equal = a.filter { $0 == pivot }
+  let greater = a.filter { $0 > pivot }
+  return quickSortNaive(less) + equal + quickSortNaive(greater)
+}
+
+public func partitionLomuto<T: Comparable>(_ a: inout [T],
+                                           low: Int,
+                                           high: Int) -> Int {
+  let pivot = a[high]
+  var i = low
+  for j in low..<high {
+    if a[j] <= pivot {
+      a.swapAt(i, j)
+      i += 1
+    }
+  }
+  a.swapAt(i, high)
+  return i
+}
