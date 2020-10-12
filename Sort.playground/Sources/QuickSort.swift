e8e41653ed0d958d9bb6734dbc892560e44b5de8
Lucas <lucaspedrazoli@gmail.com>
2020-10-12 18:10:02 -0300

    add partition hoare

--- Sort.playground/Sources/QuickSort.swift
+++ Sort.playground/Sources/QuickSort.swift
@@ -36,4 +36,22 @@ public func quickSortLomuto<T: Comparable>(_ a: inout [T],
   }
 }
 
+public func partitionHoare<T: Comparable>(_ a: inout [T],
+                                          low: Int,
+                                          high: Int) -> Int {
+  let pivot = a[low]
+  var i = low - 1
+  var j = high + 1
+  while true {
+    repeat { j -= 1 } while a[j] > pivot
+    repeat { i += 1 } while a[i] < pivot
+
+    if i < j {
+      a.swapAt(i, j)
+    } else {
+      return j
+    }
+  }
+}
+
 
